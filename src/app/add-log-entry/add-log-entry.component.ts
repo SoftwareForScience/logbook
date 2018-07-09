1891dbccf23e5a69f80abb7b6dc548899ff582d4
antonioelrey <anthonyleroyy@gmail.com>
2018-07-09 14:02:49 +0200

    updated log entry

--- src/app/add-log-entry/add-log-entry.component.ts
+++ src/app/add-log-entry/add-log-entry.component.ts
@@ -8,6 +8,65 @@ import { IOptionsValues } from 'selenium-webdriver/chrome';
 })
 export class AddLogEntryComponent implements OnInit {
 
+  selectedOptions_subs;
+  selectedOptions_type;
+  options_subs = [
+    {
+      "label": "CTP",
+      "id": 1
+    },
+    {
+      "label": "DCS",
+      "id": 2
+    },
+    {
+      "label": "ECS",
+      "id": 3
+    },
+    {
+      "label": "HLT",
+      "id": 4
+    },
+    {
+      "label": "TRG",
+      "id": 5
+    }
+  ];
+
+  options_type = [
+    {
+      "label": "CAVERN",
+      "id": 1
+    },
+    {
+      "label": "DCS",
+      "id": 2
+    },
+    {
+      "label": "DQL/QA",
+      "id": 3
+    },
+    {
+      "label": "EOS",
+      "id": 4
+    },
+    {
+      "label": "GENERAL",
+      "id": 5
+    },
+    {
+      "label": "EOS",
+      "id": 6
+    },
+    {
+      "label": "HARDWARE",
+      "id": 7
+    },
+    {
+      "label": "SOFTWARE",
+      "id": 8
+    }
+  ];
   
   constructor() { }
 
