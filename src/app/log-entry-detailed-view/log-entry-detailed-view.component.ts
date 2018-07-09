b4a13cf42ca62bc70170b6cb1a82f6e41bbe1f02
CyrilMlb <malherbe@et.esiea.fr>
2018-07-09 09:28:47 +0200

    modal window to see log entries details

--- src/app/log-entry-detailed-view/log-entry-detailed-view.component.ts
+++ src/app/log-entry-detailed-view/log-entry-detailed-view.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {SuiModalService, TemplateModalConfig, ModalTemplate} from 'ng2-semantic-ui';
+import {ViewChild, Component} from '@angular/core'
+
+export interface IContext {
+    data:string;
+}
 
 @Component({
   selector: 'app-log-entry-detailed-view',
   templateUrl: './log-entry-detailed-view.component.html',
   styleUrls: ['./log-entry-detailed-view.component.css']
 })
-export class LogEntryDetailedViewComponent implements OnInit {
 
-  constructor() { }
+export class LogEntryDetailedViewComponent {
+    @ViewChild('modalTemplate')
+    public modalTemplate:ModalTemplate<IContext, string, string>
 
-  ngOnInit() {
+    constructor(public modalService:SuiModalService) {}
+
+    public open(dynamicContent:string = "Example") {
+      const config = new TemplateModalConfig<IContext, string, string>(this.modalTemplate);
+  
+      config.closeResult = "closed!";
+      config.context = { data: dynamicContent };
+  
+      this.modalService
+          .open(config)
+          .onApprove(result => { /* approve callback */ })
+          .onDeny(result => { /* deny callback */});
   }
-
 }
