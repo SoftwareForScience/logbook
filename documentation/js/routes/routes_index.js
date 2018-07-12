56730d733f51bcc8678834a0ab9c6aeadb7b03bf
CyrilMlb <malherbe@et.esiea.fr>
2018-07-12 15:01:06 +0200

    added documentation support

---
+++ documentation/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"kind":"component","component":"HomeComponent","path":"home"},{"kind":"component","component":"AddLogEntryComponent","path":"add-log-entry"},{"kind":"component","component":"PageNotFoundComponent","path":"**"}]}
